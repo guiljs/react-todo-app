85da52f1b77e8dd3f63ba530fdd7f36a4a2f2297
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-06-03 23:56:47 -0300

    Componente PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default prop => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+        <h2>
+            Nossa História
+        </h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, recusandae! Blanditiis assumenda ipsam aliquid adipisci eum. Laboriosam repudiandae sequi corporis tenetur maiores numquam fugiat, repellendus placeat harum exercitationem error accusamus!</p>
+        <h2>
+            Missão e Visão
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iste commodi est voluptatem? Voluptatem exercitationem fugit hic maiores! Placeat dolorem cumque quasi officia labore repudiandae consequatur voluptate, perferendis quos inventore?</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima porro itaque maxime, accusantium modi perspiciatis, omnis accusamus explicabo animi culpa sed quas molestias ea, laborum architecto cumque sapiente laboriosam repellat.</p>
+        <h2>
+            Imprensa
+        </h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem optio sunt iure nihil necessitatibus possimus, perferendis delectus quod sint odio vero vitae saepe ullam laborum mollitia eos et ducimus sapiente.</p>
     </div>
 )
