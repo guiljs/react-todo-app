0ab3117d24cac77db8297af6edd94d8522aa8144
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-06-06 08:49:42 -0300

    Consulta e exclusão de TODOS

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -16,7 +16,15 @@ export default class Todo extends Component {
 
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
     }
+    refresh() {
+        axios.get(`${URL}?sort=createdAt`)
+            .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
+    }
+
     handleChange(e) {
         this.setState({ ...this.state, description: e.target.value })
     }
@@ -24,14 +32,19 @@ export default class Todo extends Component {
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('Funcionou a requisição'))
+            .then(resp => this.refresh())
+
+    }
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
                 <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange} />
-                <TodoList />
+                <TodoList list={this.state.list} handleRemove={this.handleRemove} />
             </div>
         )
     }
