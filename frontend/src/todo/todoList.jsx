370c4c715b97e49c027afa54ef0917582ede0b96
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-06-09 10:29:37 -0300

    Conectar o TodoList com o Redux

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import IconButton from '../template/iconButton'
+import { connect } from 'react-redux'
 
-export default props => {
+import IconButton from '../template/iconButton'
+import Todo from './todo';
+
+const TodoList = props => {
     const renderRows = () => {
         const list = props.list || []
 
@@ -34,3 +37,6 @@ export default props => {
         </table>
     )
 }
+
+const mapStateToProps = state => ({ list: state.todo.list })
+export default connect(mapStateToProps)(TodoList)
